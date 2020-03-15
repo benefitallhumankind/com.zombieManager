c7b387fc6e4b0278891e1db729cf5f89c959bf7b
TKLEE <benefitallhumankind@gmail.com>
2020-03-15 16:04:04 +0900

    upload

---
+++ src/test/java/com/zombiegame/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zombiegame.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
